247d54845a633fe624240bd541730ec769d944fc
Paulo Sousa <paulopgss@gmail.com>
2020-12-02 15:47:35 -0300

    Definida estilização do global

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import './App.css'
+import Global from './global'
 
 function App() {
   return (
     <div className="App">
+      <Global />
       <h1>Hello world!</h1>
     </div>
   );
