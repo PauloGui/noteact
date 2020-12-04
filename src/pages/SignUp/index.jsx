ad7cc22ccfa62f801cc9a390bf3039e8ba620a8c
Paulo Sousa <paulopgss@gmail.com>
2020-12-04 11:21:17 -0300

    Setando estados para signup

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Box, Logo, Span, Input, LinkA, Button } from './style'
 import ImgLogo from '../../assets/logo.png'
+import { useAuth } from '../../hooks/AuthProvider'
 
 function SignUp({ history }) {
+  const { setAuthUser } = useAuth()
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [comparePass, setComparePass] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const signUp = () => {
+    setLoading(true)
+    if(!name || !email || !password || !comparePass) {
+      return alert('Todos os campos devem ser preenchidos')
+    } if (password !== comparePass) {
+      return alert('As senhas devem ser iguais.')
+    }
+  }
   return (
     <Container>
       <Box>
