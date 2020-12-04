903f5854b5531f3415c9856a80aa512aa1220d38
Paulo Sousa <paulopgss@gmail.com>
2020-12-04 12:02:55 -0300

    SignUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Container, Box, Logo, Span, Input, LinkA, Button } from './style'
 import ImgLogo from '../../assets/logo.png'
+import api from '../../services/api'
 import { useAuth } from '../../hooks/AuthProvider'
 
 function SignUp({ history }) {
@@ -9,17 +10,34 @@ function SignUp({ history }) {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [comparePass, setComparePass] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   const [loading, setLoading] = useState(false)
 
-  const signUp = () => {
+  const addUser = () => {
     setLoading(true)
-    if(!name || !email || !password || !comparePass) {
-      return alert('Todos os campos devem ser preenchidos')
-    } if (password !== comparePass) {
-      return alert('As senhas devem ser iguais.')
+    if (!name || !email || !password || !confirmPassword) {
+      setLoading(false)
+      return alert('Todos os campos são obrigatórios')
+    } if (password !== confirmPassword) {
+      setLoading(false)
+      return alert('As senhas devem ser iguais')
     }
+
+    api.post('/users', { name, email, password, confirmPassword }).then(resp => {
+      if (resp.data.success) {
+        setLoading(false)
+        setAuthUser({ authenticated: true, token: resp.data.auth })
+        localStorage.setItem('@noteact_token', resp.data.auth)
+        return
+      }
+      alert(resp.data.message)
+
+    }).catch((err) => {
+      alert('Erro ao cadastrar usuário')
+    })
   }
+
+  
   return (
     <Container>
       <Box>
@@ -27,11 +45,11 @@ function SignUp({ history }) {
           <Logo src={ImgLogo} />
         </LinkA>
         <Span>Assuma o controle de suas anotações.</Span>
-        <Input placeholder="Nome" />
-        <Input placeholder="Email" />
-        <Input placeholder="Senha" />
-        <Input placeholder="Confirme a senha" />
-        <Button>Criar conta</Button>
+        <Input value={name} onChange={e => setName(e.target.value)} placeholder="Nome" />
+        <Input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
+        <Input value={password} onChange={e => setPassword(e.target.value)} placeholder="Senha" type="password" />
+        <Input value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="Confirme a senha" type="password" />
+        <Button onClick={addUser} >{loading ? 'Carregando...' : 'Criar'}</Button>
         <LinkA onClick={() => history.push('/signin')}>Fazer login</LinkA>
       </Box>
     </Container>
